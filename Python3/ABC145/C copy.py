b1cf8937f68c44aa6a2a911c4468e6fd0e08ec1f
y0sh-psys-Win10 <y0sh.webapp@gmail.com>
2019-11-21 19:49:36 +0900

    add

--- Python3/ABC145/C copy.py
+++ Python3/ABC145/C copy.py
@@ -1,8 +1,4 @@
-import numpy as np
-from scipy.spatial import distance
-from scipy.sparse.csgraph import floyd_warshall
-n = int(input())
-xy = np.zeros((n, 2))
-for i in range(n):
-    xy[i, 0], xy[i, 1] = (int(x) for x in input().split())
-print(np.sum(distance.cdist(xy, xy, metric='euclidean'))/n)
+a = [list(map(int,input().split())) for i in range(3)]
+# b = [input().split()]
+print(a)
+# print(b)
