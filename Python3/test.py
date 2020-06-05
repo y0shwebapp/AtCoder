ce0010d07e638841b407c7b24640c3ae016d6eb4
psystoki <psys.toki@gmail.com>
2020-06-05 19:46:01 +0900

    a

--- Python3/test.py
+++ Python3/test.py
@@ -1,21 +1,19 @@
-n = int(input())
-a = []
-for _ in range(n):
-    a.append(int(input()))
+N = int(input())
 
+def calc(x):
+    if x % 3 == 0:
+        return True
+    for i in str(x):
+        if i == '3':
+            return True
+    return False
 
+ret = []
+for i in range(1, N+ 1):
+    if calc(i):
+        ret.append(i)
 
-b = 1
-s = 0
-move = []
-
-while True :
-    move.append(b)
-    b = a[b - 1]
-    s += 1
-    if b == 2 :
-        print(s)
-        exit(0)
-    if n < s :
-        print(-1)
-        exit(0)
+ret2 = ''
+for r in ret:
+    ret2 = ret2 + ' ' + str(r)
+print(ret2)
